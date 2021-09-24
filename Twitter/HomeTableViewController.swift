96d35b1b2d267fc98366e363585f343e7327dfdb
Brang Mai <brangmai100@gmail.com>
2021-09-24 12:49:27 -0400

    Added features: load more tweets, refresh the screen for newer tweets, were added to the projects

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -11,26 +11,40 @@ import UIKit
 class HomeTableViewController: UITableViewController {
     
     var tweetArray = [NSDictionary]()
-    var numberOfTweet: Int!
+    var numberOfTweet: Int! = 20
+    let myrefeshControl = UIRefreshControl()
 
-    func loadTweet(){
+    @objc func loadTweets(){
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let myParams = ["count": 10]
+        let myParams = ["count": numberOfTweet]
         TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
             self.tweetArray.removeAll()
             for tweet in tweets {
                 self.tweetArray.append(tweet)
             }
             self.tableView.reloadData()
+            self.myrefeshControl.endRefreshing()
         }, failure: { (Error) in
             print("Can not retreive tweets!")
         })
     }
     
+    func loadMoreTweets() {
+        numberOfTweet += 20
+        loadTweets()
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1 == tweetArray.count {
+            loadMoreTweets()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweet()
-
+        loadTweets()
+        myrefeshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+        tableView.refreshControl = myrefeshControl
     }
 
     @IBAction func onLogout(_ sender: Any) {
@@ -59,12 +73,12 @@ class HomeTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+        //return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        // return the number of rows
         return tweetArray.count
     }
 
